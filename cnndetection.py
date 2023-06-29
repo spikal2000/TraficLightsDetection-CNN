c010ae4af8dd79494b58d064375634f34d5d5b68
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2023-06-29 18:28:00 +0300

    changes

--- cnndetection.py
+++ cnndetection.py
@@ -19,26 +19,42 @@ def load_data(data_dir, num_classes):
         for filename in glob.glob(os.path.join(data_dir, str(class_id), '*.png')):
             print(f"Loading {filename}")  # Add this line to print out each file being loaded
             im = Image.open(filename)
+            im = im.resize((32, 32))
             im = np.array(im)
             X.append(im)
             Y.append(class_id)
-    return np.array(X), np.array(Y)
+    return np.array(X, dtype='float32'), np.array(Y, dtype='int')  # Use float32 for X and int for Y
 
+def load_test_data(data_dir):
+    X = []
+    filenames = []
+
+    for filename in glob.glob(os.path.join(data_dir, '*.png')):
+        print(f"Loading {filename}")  # Add this line to print out each file being loaded
+        im = Image.open(filename)
+        im = im.resize((32, 32))
+        im = np.array(im)
+        X.append(im)
+        filenames.append(filename)
+    return np.array(X, dtype='float32'), filenames
 
 num_classes = 43  # Adjust this to match the number of road sign types in your dataset
 data_dir = '../Traffic_Sign_Dataset/Train'
-test_dir = '../Traffic_Sign_Dataset/Test'
+test_dir = '../Traffic_Sign_Dataset/Test/img'
 
 # Load the data
-X, Y = load_data(data_dir, num_classes)
-X_test, Y_test = load_data(test_dir, num_classes)
+X_test, test_filenames = load_test_data(test_dir)
 
+# Check if the validation data is loaded correctly
+if len(X_test) == 0:
+    raise Exception("No validation data found. Please check the path to the validation dataset.")
+X, Y = load_data(data_dir, num_classes)
 # Shuffle the data
 X, Y = shuffle(X, Y)
 
 # Convert labels to one-hot vectors
 Y = to_categorical(Y, num_classes)
-Y_test = to_categorical(Y_test, num_classes)
+# Y_test = to_categorical(Y, num_classes)
 
 # Real-time data preprocessing
 img_prep = ImagePreprocessing()
@@ -64,7 +80,7 @@ network = regression(network, optimizer='adam', loss='categorical_crossentropy',
 
 # Train using classifier
 model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=50, shuffle=True, validation_set=(X_test, Y_test), show_metric=True, batch_size=96, run_id='cifar10_cnn')
+model.fit(X, Y, n_epoch=50, shuffle=True, validation_set=(X_test, Y), show_metric=True, batch_size=96, run_id='cifar10_cnn')
 
 # Save the model
 model.save("road_signs_model.tfl")
