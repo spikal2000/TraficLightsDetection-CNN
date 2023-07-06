8db32cba4149c7bd58ae3b784259ca2bd36a0364
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2023-07-06 10:11:17 +0300

    remove old

--- rcnncustomTest.py
+++
@@ -1,9 +0,0 @@
-# Define the model as before
-model = fasterrcnn_resnet50_fpn(pretrained=False)
-in_features = model.roi_heads.box_predictor.cls_score.in_features
-num_classes = len(train_df['ClassId'].unique())
-model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
-# Load the model
-model.load_state_dict(torch.load('model_weights.pth'))
-model.eval()
